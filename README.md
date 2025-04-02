a00a3f9dcdfb517f22680196a14605daeea24071
r1mahar <pcrmahar@gmail.com>
2025-04-02 23:28:11 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,17 @@
+❏ `BOT_TOKEN`  - Get Bot Token From [@BotFather](https://t.me/BotFather)
+
+❏ `API_ID` - From [my.telegram.org ](https://my.telegram.org/)
+
+❏ `API_HASH` - From [my.telegram.org](https://my.telegram.org/)
+
+❏ `PORT` - Any Port Like 6969 | Dont Use 8080 in [app.koyeb.com]
+
+## Botfather Commands
+```
+start - 🦋 Checking Bot Live Or Not
+stop - 🔥 Stop Uploading Process
+rajesh - ➕ Upload Your Downloaded Txt File
+mahar - ➕ Upload Your Downloaded Txt File
+txt - ➕ Upload Your Downloaded Txt File
+txt1 - ➕ Upload Your Downloaded Txt File
+```
---
+++ utils.py
@@ -0,0 +1,113 @@
+import random
+import time
+import math
+import os
+from pyrogram.errors import FloodWait
+
+class Timer:
+    def __init__(self, time_between=5):
+        self.start_time = time.time()
+        self.time_between = time_between
+
+    def can_send(self):
+        if time.time() > (self.start_time + self.time_between):
+            self.start_time = time.time()
+            return True
+        return False
+
+
+from datetime import datetime,timedelta
+
+def hrb(value, digits= 2, delim= "", postfix=""):
+    """Return a human-readable file size.
+    """
+    if value is None:
+        return None
+    chosen_unit = "B"
+    for unit in ("KiB", "MiB", "GiB", "TiB"):
+        if value > 1000:
+            value /= 1024
+            chosen_unit = unit
+        else:
+            break
+    return f"{value:.{digits}f}" + delim + chosen_unit + postfix
+
+def hrt(seconds, precision = 0):
+    """Return a human-readable time delta as a string.
+    """
+    pieces = []
+    value = timedelta(seconds=seconds)
+    
+
+    if value.days:
+        pieces.append(f"{value.days}d")
+
+    seconds = value.seconds
+
+    if seconds >= 3600:
+        hours = int(seconds / 3600)
+        pieces.append(f"{hours}h")
+        seconds -= hours * 3600
+
+    if seconds >= 60:
+        minutes = int(seconds / 60)
+        pieces.append(f"{minutes}m")
+        seconds -= minutes * 60
+
+    if seconds > 0 or not pieces:
+        pieces.append(f"{seconds}s")
+
+    if not precision:
+        return "".join(pieces)
+
+    return "".join(pieces[:precision])
+
+
+
+timer = Timer()
+
+async def progress_bar(current, total, reply, start):
+    if timer.can_send():
+        now = time.time()
+        diff = now - start
+        if diff < 1:
+            return
+        else:
+            perc = f"{current * 100 / total:.1f}%"
+            elapsed_time = round(diff)
+            speed = current / elapsed_time
+            remaining_bytes = total - current
+            if speed > 0:
+                eta_seconds = remaining_bytes / speed
+                eta = hrt(eta_seconds, precision=1)
+            else:
+                eta = "-"
+            sp = str(hrb(speed)) + "/s"
+            tot = hrb(total)
+            cur = hrb(current)
+            bar_length = 10
+            completed_length = int(current * bar_length / total)
+            remaining_length = bar_length - completed_length
+            
+            symbol_pairs = [
+                ("😁", "😡"),
+                ("❤️", "🤍"),
+                ("👍", "👎"),
+                ("🪷", "🥀"),
+                ("🔴", "⚪"),
+                ("♦", "◇"),
+                ("🟥", "⬜"),
+                ("✔️", "✖️"),
+                ("🔆", "🔅"),
+                ("💗", "💌")
+            ]
+            chosen_pair = random.choice(symbol_pairs)
+            completed_symbol, remaining_symbol = chosen_pair
+
+            progress_bar = completed_symbol * completed_length + remaining_symbol * remaining_length
+            
+            try:
+                await reply.edit(f'<b>\n ╭───────────────────────╮\n│            **__UPLOADING YOUR FILE__**\n├───────────────────────\n├⚡ {progress_bar}\n├♦ Precent » {perc} \n├🚀 Speed » {sp} \n├📟 Processed » {cur}\n├🧲 Size » {tot}\n├🕐 ETA » {eta} \n├🤖 Bot Made By » Rajesh Mahar 🕊️🕊️\n╰───────────────────────╯\n</b>') 
+            except FloodWait as e:
+                time.sleep(e.x)
+
